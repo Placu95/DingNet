bdbd93b83da810247946791e5bc86d1ed65853ac
Placu <andrea.placuzzi@studio.unibo.it>
2020-04-02 15:26:51 +0200

    update dependency

--- buildSrc/src/main/kotlin/ExternalLib.kt
+++ buildSrc/src/main/kotlin/ExternalLib.kt
@@ -3,7 +3,7 @@ data class ExternalLib(val libBaseName: String, val libVersion: String, val libU
     val libFullName = "$libBaseName-$libVersion.jar"
 
     companion object {
-        val mqtt_client_wrapper: ExternalLib = ExternalLib("MqttClientWrapper", "0.3.2",
-            "https://github.com/Placu95/MqttClientWrapper/releases/download/v0.3.2/MqttClientWrapper-0.3.2.jar")
+        val mqtt_client_wrapper: ExternalLib = ExternalLib("MqttClientWrapper", "0.3.3",
+            "https://github.com/Placu95/MqttClientWrapper/releases/download/v0.3.3/MqttClientWrapper-0.3.3.jar")
     }
 }
