5ddecac3b3ad6791a8ee02400b2d66873765020f
Placu <andrea.placuzzi@studio.unibo.it>
2020-04-10 16:19:16 +0200

    compute communication delay

--- src/main/kotlin/it/unibo/acdingnet/protelis/physicalnetwork/PhysicalNetwork.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/physicalnetwork/PhysicalNetwork.kt
@@ -1,17 +1,19 @@
 package it.unibo.acdingnet.protelis.physicalnetwork
 
+import iot.GlobalClock
 import it.unibo.acdingnet.protelis.node.GenericNode
 import it.unibo.acdingnet.protelis.physicalnetwork.configuration.Reader
 import it.unibo.acdingnet.protelis.util.skip
 import org.protelis.lang.datatype.DeviceUID
 import util.time.DoubleTime
 import util.time.Time
-import util.time.TimeUnit
 
-class PhysicalNetwork(reader: Reader) {
+class PhysicalNetwork(reader: Reader, val clock: GlobalClock) {
 
     private val hostBroker: Host
-    private var hosts: Set<Host> = emptySet()
+    private var receivingQueueFreeFrom: Time = DoubleTime.zero()
+    private var sendingQueueFreeFrom: Time = DoubleTime.zero()
+    private var hosts: Set<Host>
     private val configurationNetwork = reader.configurationNetwork
 
     init {
@@ -53,23 +55,42 @@ class PhysicalNetwork(reader: Reader) {
     private fun hostByType(hostType: HostType) = checkNotNull(hosts.find { it.type == hostType })
 
     fun delayToPublish(deviceUID: DeviceUID, messageLenght: Int): Time =
-        computeDelay(getHostByDevice(deviceUID), hostBroker)
+        computeDelay(getHostByDevice(deviceUID), hostBroker, receivingQueueFreeFrom, messageLenght)
 
     fun delayToReceive(deviceUID: DeviceUID, messageLenght: Int): Time =
-        computeDelay(hostBroker, getHostByDevice(deviceUID))
+        computeDelay(hostBroker, getHostByDevice(deviceUID), sendingQueueFreeFrom, messageLenght)
 
     private fun getHostByDevice(deviceUID: DeviceUID) =
         checkNotNull(hosts.find { it.devices.contains(deviceUID) })
 
-    private fun computeDelay(from: Host, to: Host): Time {
-        if (from == to) {
-            return DoubleTime(0.5, TimeUnit.MILLIS)
-        }
-        var delay = computeDelay(from)
-
-        // TODO finish to compute delay
-        return delay
+    private fun computeDelay(h1: Host, h2: Host, queueTime: Time, messageLenght: Int): Time {
+        return maxTime(queueTime, clock.time)
+            .plusMillis(messageLenght / hostBroker.getBandwidth()) // they are millis???
+            .plus(computeRTTBetweenHosts(h1, h2))
     }
 
-    private fun computeDelay(from: Host): Time = TODO()
+    private fun maxTime(t1: Time, t2: Time) = if (t1.isAfter(t2)) t1 else t2
+
+    private fun computeRTTBetweenHosts(h1: Host, h2: Host): Time {
+        if (h1 == h2) {
+            return configurationNetwork.dHostBroker
+        }
+        return when (h1.type) {
+            HostType.CLOUD -> when (h2.type) {
+                HostType.CLOUD -> configurationNetwork.dcc
+                HostType.EDGE -> configurationNetwork.dec
+                HostType.SMARTPHONE -> configurationNetwork.dsc
+            }
+            HostType.EDGE -> when (h2.type) {
+                HostType.CLOUD -> configurationNetwork.dec
+                HostType.EDGE -> configurationNetwork.dee
+                HostType.SMARTPHONE -> configurationNetwork.dsc + configurationNetwork.dec
+            }
+            HostType.SMARTPHONE -> when (h2.type) {
+                HostType.CLOUD -> configurationNetwork.dsc
+                HostType.EDGE -> configurationNetwork.dsc + configurationNetwork.dec
+                else -> throw IllegalStateException()
+            }
+        }
+    }
 }
