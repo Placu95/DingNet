97261475c06e56a4fc693ec62a3dee7a41622f74
Placu <andrea.placuzzi@studio.unibo.it>
2020-04-22 15:39:28 +0200

    add distinct

--- src/main/kotlin/it/unibo/acdingnet/protelis/util/Utils.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/util/Utils.kt
@@ -1,5 +1,7 @@
 package it.unibo.acdingnet.protelis.util
 
+import org.protelis.lang.datatype.Tuple
+import org.protelis.lang.datatype.impl.ArrayTupleImpl
 import java.math.RoundingMode
 
 object Utils {
@@ -8,4 +10,11 @@ object Utils {
     @JvmStatic
     fun roundToDecimal(value: Double, numOfDecimal: Int = 1) = value.toBigDecimal()
         .setScale(numOfDecimal, RoundingMode.HALF_EVEN).toDouble()
+
+    @JvmStatic
+    fun distinctByUID(tuple: Tuple): Tuple {
+        var ret: Tuple = ArrayTupleImpl()
+        tuple.distinctBy { (it as ArrayTupleImpl)[0] }.forEach { ret = ret.append(it) }
+        return ret
+    }
 }
