b66a6517c6946f2c2d0d9821a6cedc92b5d4e10d
Placu <andrea.placuzzi@studio.unibo.it>
2020-04-23 16:26:46 +0200

    change protelis program to execute

--- src/main/kotlin/it/unibo/acdingnet/protelis/application/Acsos.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/application/Acsos.kt
@@ -35,7 +35,7 @@ import java.util.*
 class Acsos(
     motes: List<Mote>,
     timer: GlobalClock,
-    protelisProgram: String = "protelis:homeHeating_timer"
+    protelisProgram: String = "protelis:homeHeating_timer_v2"
 ) :
     ProtelisApplication(motes, timer, protelisProgram, emptyList()) {
 
