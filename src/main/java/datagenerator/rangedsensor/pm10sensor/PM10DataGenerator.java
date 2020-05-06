9cf223f2422a14c57b90113e2e58a451ca80b851
Placu <andrea.placuzzi@studio.unibo.it>
2020-05-06 18:43:38 +0200

    add pm10 file configuration for acsos

--- src/main/java/datagenerator/rangedsensor/pm10sensor/PM10DataGenerator.java
+++ src/main/java/datagenerator/rangedsensor/pm10sensor/PM10DataGenerator.java
@@ -4,7 +4,7 @@ import datagenerator.rangedsensor.abstractimpl.RangeDataGenerator;
 
 public class PM10DataGenerator extends RangeDataGenerator {
 
-    private static final String DEFAULT_CONFIG_FILE = "/sensorsConfigurations/pm10SensorConfig.toml";
+    private static final String DEFAULT_CONFIG_FILE = "/sensorsConfigurations/pm10SensorConfigAcsos2020.toml";
     private final String configFile;
 
     public PM10DataGenerator() {
