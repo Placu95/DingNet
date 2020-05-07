65f108a5c4f7b02b8c2a28ff6cf13d0a0647811f
Placu <andrea.placuzzi@studio.unibo.it>
2020-05-07 17:17:00 +0200

    store of lora transmissions

--- src/main/kotlin/it/unibo/acdingnet/protelis/dingnetwrapper/SensorECForDingNet.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/dingnetwrapper/SensorECForDingNet.kt
@@ -2,10 +2,14 @@ package it.unibo.acdingnet.protelis.dingnetwrapper
 
 import iot.GlobalClock
 import it.unibo.acdingnet.protelis.executioncontext.SensorExecutionContext
+import it.unibo.acdingnet.protelis.model.LoRaTransmission
+import it.unibo.acdingnet.protelis.physicalnetwork.NetworkStatistic
 import it.unibo.acdingnet.protelis.util.Const
 import it.unibo.acdingnet.protelis.util.Const.DEFAULT_IAQ_LEVEL
 import it.unibo.mqttclientwrapper.api.MqttClientBasicApi
 import org.protelis.vm.NetworkManager
+import util.time.DoubleTime
+import util.time.TimeUnit
 
 class SensorECForDingNet(
     private val sensorNode: SensorNodeWrapper,
@@ -23,5 +27,10 @@ class SensorECForDingNet(
 
     override fun getCurrentTime(): Number {
         return timer.time.asSecond()
+    }
+
+    override fun handleDeviceTransmission(message: LoRaTransmission) {
+        NetworkStatistic.addDelay(DoubleTime(message.timeOnAir, TimeUnit.MILLIS), NetworkStatistic.Type.UPLOAD)
+        super.handleDeviceTransmission(message)
     }
 }
