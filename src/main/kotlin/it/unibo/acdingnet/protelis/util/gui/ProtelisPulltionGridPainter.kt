f2075b9667548a5e34954cf77c0ad254cf3be853
Placu <andrea.placuzzi@studio.unibo.it>
2020-05-11 20:45:41 +0200

    lower bound is inclusive

--- src/main/kotlin/it/unibo/acdingnet/protelis/util/gui/ProtelisPulltionGridPainter.kt
+++ src/main/kotlin/it/unibo/acdingnet/protelis/util/gui/ProtelisPulltionGridPainter.kt
@@ -8,10 +8,10 @@ class ProtelisPulltionGridPainter(pollutionGrid: PollutionGrid) :
     PollutionGridPainter(pollutionGrid) {
 
     override fun getColor(airQuality: Float): Color = when {
-        airQuality > 100 -> Color.decode("#e8416f")
-        airQuality > 75 -> Color.decode("#f29305")
-        airQuality > 50 -> Color.decode("#eec20b")
-        airQuality > 25 -> Color.decode("#c0e010")
+        airQuality >= 100 -> Color.decode("#e8416f")
+        airQuality >= 75 -> Color.decode("#f29305")
+        airQuality >= 50 -> Color.decode("#eec20b")
+        airQuality >= 25 -> Color.decode("#c0e010")
         else -> Color.decode("#4edc2e")
     }
 }
